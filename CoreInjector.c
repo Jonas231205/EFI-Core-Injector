a443a7d06e057f23f479654481bb80bb4204560b
Jonas231205 <110816772+Jonas231205@users.noreply.github.com>
2024-03-28 11:57:03 +0100

    Add files via upload
    CORE-NJECTOR (Main.c)

---
+++ CoreInjector.c
@@ -0,0 +1,75 @@
+#include <efi.h>
+#include <efilib.h>
+
+// Define the address for ICR (Interrupt Command Register) in Local APIC
+#define APIC_ICR_ADDRESS 0xFEE00300
+#define GNU_EFI_USE_MS_ABI 1
+#define COMMAND_MAGIC baseOperation*0x7346
+
+// Function to introduce a delay
+void delay(UINTN microseconds) {
+    // Estimate number of iterations required for the specified delay
+    // This estimate might need adjustment depending on the processor speed. 100000 is about 1 sec
+    UINTN iterations = microseconds * 1000;
+
+    // Simple loop to introduce a delay
+    for (UINTN i = 0; i < iterations; i++) {
+        __asm__ volatile ("nop");
+    }
+}
+
+// EFI entry point
+EFI_STATUS 
+efi_main(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+    // Initialize internal GNU-EFI functions
+    InitializeLib(ImageHandle, SystemTable);
+
+    // Get handle to this image
+    EFI_LOADED_IMAGE *LoadedImage = NULL;
+    EFI_STATUS status = uefi_call_wrapper(BS->OpenProtocol, 6, ImageHandle, &gEfiLoadedImageProtocolGuid,
+                                        (VOID**)&LoadedImage, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+	/* Function to TODO:
+		1. Detect number of physical processor
+		
+		2. Replace the variable to Number of core then sending IPI Instructions.
+		
+		3. Windows partition finder
+		
+		4. Bcd data load and config number of proc check and changer.
+		
+		5. Detect Number of RAM and setting max memory on windows os.
+		
+		6. Start Address for the processor setting to the start address of the blocked list from bcd data.
+			
+		7. Map some code to the specific start address.
+		
+		8. After mapping unload the driver and call windows bootloader.
+		
+		*/
+
+    // Send IPI to the target processor
+    UINT32 StartAddress = 0x2000; // Start address for the 6th processor
+    UINT32 IpiCommand = 0x000C4600 + StartAddress; // Indicates the start address
+
+    // Send IPI using inline assembly
+    __asm__ volatile (
+        "movq $0xFEE00300, %%rsi\n"  // Load address of ICR into RSI
+        "movl %0, %%eax\n"            // Load IPI command into EAX
+        "movl %%eax, (%%rsi)\n"       // Send IPI command
+        :
+        : "r" (IpiCommand)
+        : "eax", "esi"
+    );
+
+    // Introduce a delay of 100 milliseconds (adjust as needed)
+    delay(100000);
+
+    // Print "Hello, World!"
+    Print(L"Hello, World!\n");
+
+    // Return to the calling shell
+    uefi_call_wrapper(BS->Exit, 4, ImageHandle, EFI_SUCCESS, 0, NULL);
+
+    // This point should never be reached
+    return EFI_SUCCESS;
+}
